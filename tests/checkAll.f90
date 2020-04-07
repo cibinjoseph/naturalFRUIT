595d5dd2d619ed5e09d6de2c14ad39a13ec332eb
cibinjoseph <cibinjoseph92@gmail.com>
2020-04-07 18:26:15 +0530

    Add unit tests

--- tests/checkAll.f90
+++ tests/checkAll.f90
@@ -2,11 +2,12 @@ program checkAll
   use fruit
   implicit none
 
-  integer, parameter :: nAsserts = 1
-  logical, dimension(nAsserts) :: isCorrect
+  integer, parameter :: nAsserts = 2
+  logical, dimension(nAsserts) :: stat
   
   print*, "Checking assert_equal"
-  call assert_equal(.true., .true.)
-  print*
+  call assert_equal(.true., .true., status=stat(1))
+  call assert_equal(.true., .false., status=stat(2))
+  print*, stat
   print*, "Completed checks"
 end program checkAll
