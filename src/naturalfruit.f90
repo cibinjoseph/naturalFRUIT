250f9ba80f41f9289aa7f887ce0ce7661469d473
cibinjoseph <cibinjoseph92@gmail.com>
2020-04-07 18:35:19 +0530

    Remove deprecated equal subroutine from fruit_util

--- src/naturalfruit.f90
+++ src/naturalfruit.f90
@@ -26,18 +26,7 @@ module fruit_util
   !! A utility module for [[fruit]] to use.
   private
 
-  public :: equal, to_s, strip
-
-  interface equal
-    !! category: fruit_util
-    !! Check equality
-    module procedure equalEpsilon
-    module procedure floatEqual
-    module procedure integerEqual
-    module procedure doublePrecisionEqual
-    module procedure stringEqual
-    module procedure logicalEqual
-  end interface
+  public :: to_s, strip
 
   interface to_s
     !! Convert to string
@@ -143,118 +132,6 @@ contains
     character(len= length):: strip_length_
     strip_length_ = trim(adjustl(value))
   end function strip_length_
-
-  !------------------------
-  ! test if 2 values are close
-  !------------------------
-  !logical function equal (number1, number2) 
-  !  real,  intent (in) :: number1, number2
-  !  
-  !  return equalEpsilon (number1, number2, epsilon(number1))
-  !
-  !end function equal
-
-
-  function equalEpsilon (number1, number2, epsilon ) result (resultValue)
-    !! Check equality of reals within a certain tolerance
-    real , intent (in) :: number1, number2, epsilon 
-    logical :: resultValue 
-
-    resultValue = .false.
-
-    ! test very small number1
-    if ( abs(number1) < epsilon .and.  abs(number1 - number2) < epsilon ) then
-      resultValue = .true.
-    else 
-      if ((abs(( number1 - number2)) / number1) < epsilon ) then
-        resultValue = .true.
-      else
-        resultValue = .false.
-      end if
-    end if
-
-  end function equalEpsilon
-
-  function floatEqual (number1, number2 ) result (resultValue)
-    !! Check equality of reals within a default tolerance
-    real , intent (in) :: number1, number2
-    real :: epsilon 
-    logical :: resultValue 
-
-    resultValue = .false.
-    epsilon = 1E-6
-
-    ! test very small number1
-    if ( abs(number1) < epsilon .and.  abs(number1 - number2) < epsilon ) then
-      resultValue = .true.
-    else 
-      if ((abs(( number1 - number2)) / number1) < epsilon ) then
-        resultValue = .true.
-      else
-        resultValue = .false.
-      end if
-    end if
-  end function floatEqual
-
-  function doublePrecisionEqual (number1, number2 ) result (resultValue)
-    !! Check equality of double within a default tolerance
-    double precision , intent (in) :: number1, number2
-    real :: epsilon 
-    logical :: resultValue 
-
-    resultValue = .false.
-    epsilon = 1E-6
-    !epsilon = epsilon (number1)
-
-    ! test very small number1
-    if ( abs(number1) < epsilon .and.  abs(number1 - number2) < epsilon ) then
-      resultValue = .true.
-    else 
-      if ((abs(( number1 - number2)) / number1) < epsilon ) then
-        resultValue = .true.
-      else
-        resultValue = .false.
-      end if
-    end if
-  end function doublePrecisionEqual
-
-  function integerEqual (number1, number2 ) result (resultValue)
-    !! Check equality of integers
-    integer , intent (in) :: number1, number2
-    logical :: resultValue 
-
-    resultValue = .false.
-
-    if ( number1 .eq. number2 ) then
-      resultValue = .true.
-    else 
-      resultValue = .false.
-    end if
-  end function integerEqual
-
-  function stringEqual (str1, str2 ) result (resultValue)
-    !! Check equality of strings
-    character(*) , intent (in) :: str1, str2
-    logical :: resultValue 
-
-    resultValue = .false.
-
-    if ( str1 .eq. str2 ) then
-      resultValue = .true.
-    end if
-  end function stringEqual
-
-  function logicalEqual (l1, l2 ) result (resultValue)
-    !! Check equality of logicals
-    logical, intent (in) :: l1, l2
-    logical              :: resultValue 
-
-    resultValue = .false.
-
-    if ( l1 .eqv. l2 ) then
-      resultValue = .true.
-    end if
-  end function logicalEqual
 end module fruit_util
 
 
