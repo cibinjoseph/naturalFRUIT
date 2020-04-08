5d60c5a47e1c7f2f5905b90dd06391cefc58a433
cibinjoseph <cibinjoseph92@gmail.com>
2020-04-08 14:26:54 +0530

    Update tests

--- tests/checkAll.f90
+++ tests/checkAll.f90
@@ -6,8 +6,9 @@ program checkAll
   logical, dimension(nAsserts) :: stat
   
   print*, "Checking assert_equal"
-  call assert_equal(.true., .true.)
-  call assert_equal((/.true., .true./),(/.true., .true./))
-  ! print*, stat
+  call assert_equal(.true., .true., status=stat(1))
+  ! call assert_equal(.true., .false., status=stat(2))
+  call assert_equal((/.true., .false./),(/.true., .true./), status=stat(2))
+  print*, stat
   print*, "Completed checks"
 end program checkAll
