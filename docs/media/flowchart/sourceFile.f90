adf36aac06e673ed9b4e46421052d207036940d6
cibinjoseph <cibinjoseph92@gmail.com>
2020-04-10 16:09:58 +0530

    Update docs

---
+++ docs/media/flowchart/sourceFile.f90
@@ -0,0 +1,15 @@
+module myModule
+  ! Module to be tested
+contains
+  .
+  .
+
+  ! Funtion to be tested
+  function getValue(a, b)
+    ! Do something with a and b
+    ! and return getValue
+  end function getValue
+  .
+  .
+
+end module myModule
